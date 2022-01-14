832a8b3000dd09f4e1520ffdc46e1d00ed03d04a
Wonhee Yun <56327729+wonhee3472@users.noreply.github.com>
2022-01-14 11:40:17 -0600

    saving all the queries

--- queries.sql
+++ queries.sql
@@ -56,3 +56,9 @@ CREATE TABLE titles (
     to_date DATE NOT NULL,
     FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
 );
+
+SELECT first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
