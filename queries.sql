af25fb469c66ac1161628719ce9a0920f42c6ff7
Wonhee Yun <56327729+wonhee3472@users.noreply.github.com>
2022-01-15 01:16:48 -0600

    adding new queries and a data

--- queries.sql
+++ queries.sql
@@ -62,3 +62,87 @@ INTO retirement_info
 FROM employees
 WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+-- Joining dept_employee and retiremet_info tables
+SELECT ri.emp_no,
+ri.first_name,
+ri.last_name,
+de.to_date
+INTO current_emp
+FROM retirement_info as ri
+LEFT JOIN dept_employee as de
+ON ri.emp_no=de.emp_no
+WHERE de.to_date = ('9999-01-01');
+
+-- Joining departments and dept_manager tables
+SELECT d.dept_name,
+     dm.emp_no,
+     dm.from_date,
+     dm.to_date
+FROM departments as d
+INNER JOIN dept_manager as dm
+ON d.dept_no = dm.dept_no;
+
+-- Employee count by department number
+SELECT COUNT(ce.emp_no), de.dept_no
+INTO emp_cnt_by_dept_no
+FROM current_emp as ce
+LEFT JOIN dept_employee as de
+ON ce.emp_no = de.emp_no
+GROUP BY de.dept_no
+ORDER BY de.dept_no;
+
+-- Creating a emp_info table
+SELECT e.emp_no, e.first_name, e.last_name, e.gender, s.salary, de.to_date
+INTO emp_info
+FROM employees as e
+INNER JOIN salaries as s
+ON (e.emp_no = s.emp_no)
+INNER JOIN dept_employee as de
+ON (e.emp_no = de.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (e.hire_date BETWEEN '1985-01-01' AND '1988-12-31')
+AND (de.to_date = '9999-01-01');
+
+-- Creating a manager_info table:
+SELECT  dm.dept_no,
+        d.dept_name,
+        dm.emp_no,
+        ce.last_name,
+        ce.first_name,
+        dm.from_date,
+        dm.to_date
+INTO manager_info
+FROM dept_manager AS dm
+    INNER JOIN departments AS d
+        ON (dm.dept_no = d.dept_no)
+    INNER JOIN current_emp AS ce
+        ON (dm.emp_no = ce.emp_no);
+
+
+-- Creating department retirees
+SELECT ce.emp_no, ce.first_name, ce.last_name, d.dept_name
+-- INTO dept_info
+FROM current_emp as ce
+INNER JOIN dept_employee as de
+ON (ce.emp_no = de.emp_no)
+INNER JOIN departments as d
+ON (de.dept_no = d.dept_no);
+
+-- creating a table for retirees in the sales team
+SELECT ri.emp_no, ri.first_name, ri.last_name, d.dept_name
+FROM retirement_info as ri
+INNER JOIN dept_employee as de
+ON (ri.emp_no = de.emp_no)
+INNER JOIN departments as d
+ON (de.dept_no = d.dept_no)
+WHERE (de.dept_no = 'd007');
+
+-- creating a table for retirees in both the sales team and development team
+SELECT ri.emp_no, ri.first_name, ri.last_name, d.dept_name
+FROM retirement_info as ri
+INNER JOIN dept_employee as de
+ON (ri.emp_no = de.emp_no)
+INNER JOIN departments as d
+ON (de.dept_no = d.dept_no)
+WHERE de.dept_no IN ('d007', 'd005');
