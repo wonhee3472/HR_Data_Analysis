8d508373f01d357fa15ef517a98e68f48bfd2cbf
Wonhee Yun <56327729+wonhee3472@users.noreply.github.com>
2022-01-15 22:02:28 -0600

    added a query to create a table

--- Employee_Database_challenge.sql
+++ Employee_Database_challenge.sql
@@ -17,3 +17,16 @@ FROM retirement_titles as rit
 WHERE (rit.to_date = '9999-01-01')
 ORDER BY rit.emp_no, rit.to_date DESC;
 
+-- Creating a table for mentorship_eligibility
+SELECT DISTINCT ON(e.emp_no) 
+e.emp_no, e.first_name, e.last_name, e.birth_date, 
+de.from_date, de.to_date,
+ti.title
+INTO mentorship_eligibility
+FROM employees as e
+INNER JOIN dept_employee as de
+ON (e.emp_no = de.emp_no)
+INNER JOIN titles as ti
+ON (e.emp_no = ti.emp_no)
+WHERE (de.to_date = '9999-01-01') AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY e.emp_no;
